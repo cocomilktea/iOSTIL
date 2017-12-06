3225d6ed406898c5208bf740332e27984adc9368
cocomilktea <coco_milktea@naver.com>
2017-12-06 22:40:56 +0900

    Swift struct study

---
+++ struct.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//클래스랑 비슷하지만 구조체는 상속이 안된다 
+struct Student {
+    var name: String
+    var age: Int
+    var height: Int
+}
+
+var student1 = Student(name: "Part", age: 10, height: 100)
+var student2 = Student(name: "Choi", age: 11, height: 100)
